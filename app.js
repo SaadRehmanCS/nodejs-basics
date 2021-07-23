44a0faa970b13e5090eada8c7d71bc6a3d3c61cd
saad <saadrehman2000@gmail.com>
2021-07-24 00:59:12 +0400

    minor changes

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('this is working')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('Welcome to my home page');
+    console.log(req);
+    console.log('dedde');
+    res.end();
+})
+
+server.listen(3000);
--- globals.js
+++ globals.js
@@ -3,6 +3,21 @@
 const myName = 'saad';
 const john = 'john';
 const sayHi = (name) => console.log(`hello ${name}`);
-console.log('globals file');
+
+function firstTask() {
+    console.log('task 1');
+    setTimeout(() => console.log('task 2'), 1000);
+    console.log('task 3');
+}
+firstTask();
+console.log('task 4')
+
+function taskTwo() {
+    for (let i = 0; i < 10; i++) {
+        console.log(`${i}`);
+    }
+}
+taskTwo()
+
 module.exports = sayHi;
 //console.log(module);
